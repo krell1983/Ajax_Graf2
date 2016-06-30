890531c0edaf2b90f34cd005c68d43804091328d
krell1983 <piotr.markiewicz.1983@gmail.com>
2016-06-30 18:28:40 +0200

    V 2.0

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" type="text/css" href="CSS/style.min.css" />
+        <title>AJAX</title>
+    </head>
+    <body>      
+        
+    <div class="WebPage">
+    <h1>Ajax Graf</h1>
+    <div class="Graf_Box"> 
+     <div class="Graf_Box_Grafs"> 
+     <div class="Graf NR1"></div>
+     <div class="Graf NR2"></div>
+     <div class="Graf NR3"></div>
+     <div class="Graf NR4"></div>
+     <div class="Graf NR5"></div>
+     <div class="Graf NR6"></div>
+     <div class="Graf NR7"></div>
+     <div class="Graf NR8"></div>
+     <div class="Graf NR9"></div>
+     <div class="Graf NR10"></div>
+     
+     
+     <div class="Graf NR11"></div>
+     <div class="Graf NR12"></div>
+     <div class="Graf NR13"></div>
+     <div class="Graf NR14"></div>
+     <div class="Graf NR15"></div>
+     <div class="Graf NR16"></div>
+     <div class="Graf NR17"></div>
+     <div class="Graf NR18"></div>
+        
+    </div> 
+    <div class="Graf_BoxBack_Ground_Box">    
+    <div class="ROW"> <div class="Graf_Text">100</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">90</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">80</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">70</div> </div>  
+    <div class="ROW"> <div class="Graf_Text">60</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">50</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">40</div> </div>   
+    <div class="ROW"> <div class="Graf_Text">30</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">20</div> </div> 
+    <div class="ROW"> <div class="Graf_Text">10</div> </div>   
+    </div>
+        
+        
+    </div>
+    
+    <div class="connection">Connection to server status: <div class="status">  </div></div>
+    
+    
+    </div>    
+        
+    </body>
+
+    
+    <script src="JS/jquery-2.2.3.min.js"></script>
+    <script src="JS/AJAX.min.js"></script>
+    
+</html>
